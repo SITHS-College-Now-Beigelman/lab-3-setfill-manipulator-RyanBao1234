dbd116b732d47e05439a339ad18a8ed3196d5422
RyanBao1234 <ryanb272@nycstudents.net>
2024-09-30 16:26:04 -0400

    Lab 3
    YAYYY

---
+++ Bao_Ryan_Lab_3.cpp
@@ -0,0 +1,46 @@
+//Ryan Bao
+//Lab 3
+//09/10/2024
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;                                                                                                                                                          
+
+int main()                                                                                                             //initialize function
+{
+   cout << " " << endl;                                                                                               //print empty lines so it's easier to read
+   cout << " " << endl;
+   cout << setfill('#');                                                                                              //set fill character to #
+   cout << setw(87) << " " << endl;                                                                                                                       //print # among lines
+   cout << setw(2) << " " << "                                                                                   " << setw(2) << " " << endl; 
+   cout << setw(2) << " " << "                                                                                   " << setw(2) << " " << endl;          
+   cout << setw(2) << " " << "Ways to access the Task Manager on your Windows computer:                          " << setw(2) << " " << endl;
+   cout << setw(2) << " " << "                                                                                   " << setw(2) << " " << endl;   
+   cout << setw(2) << " " << "           Press the key combination Ctrl + Shift + Escape                         " << setw(2) << " " << endl;
+   cout << setw(2) << " " << "                                                                                   " << setw(2) << " " << endl;   
+   cout << setw(2) << " " << "           Press the key combination Ctrl + Alt + Delete and select \"Task Manager\" " << setw(2) << " " << endl;
+   cout << setw(2) << " " << "                                                                                   " << setw(2) << " " << endl;   
+   cout << setw(2) << " " << "           Type \"Task Manager\" in the Windows Start menu search                    " << setw(2) << " " << endl;
+   cout << setw(2) << " " << "                                                                                   " << setw(2) << " " << endl;   
+   cout << setw(2) << " " << "                                                                                   " << setw(2) << " " << endl;
+   cout << setw(87) << ' ' << endl; 
+   cout << " " << endl;
+   cout << " " << endl;
+return 0;
+}
+/*
+######################################################################################
+#                                                                                    #
+#                                                                                    #
+# Ways to access the Task Manager on your Windows computer:                          #
+#                                                                                    #
+#            Press the key combination Ctrl + Shift + Escape                         #
+#                                                                                    #
+#            Press the key combination Ctrl + Alt + Delete and select "Task Manager" #
+#                                                                                    #
+#            Type "Task Manager" in the Windows Start menu search                    #
+#                                                                                    #
+#                                                                                    #
+######################################################################################
+*/
